1e1ea4d57e16a6074c123ed01b22ad190384329c
plewandowska777 <52824097+plewandowska777@users.noreply.github.com>
2022-02-22 15:57:05 +0100

    Ket bra (#3)
    * Add basic funcion - ket, bra, braket, ketbra, proj, vec, res, max ent state
    
    * basic function - ket, brak, braket, ketbra, proj, max ent state
    
    * add requirement numpy
    
    * installig requires numpy
    
    * Fix typo in GitHub workflow
    
    * Add basic functions with tests together with needed black, flake, isort
    
    * Add necessery package to setup.cfg
    
    * Changes suggested from PR.
    
    * PR changes.
    
    * Fixed typo
    
    * PR changes docstrings
    
    Co-authored-by: Dexter <dexter2206@gmail.com>

---
+++ quit/basicfunction.py
@@ -0,0 +1,148 @@
+import typing
+
+import numpy as np
+from scipy import linalg
+
+Array1D = typing.Union[np.ndarray, typing.Sequence[complex]]
+
+
+def ket(psi: Array1D) -> np.ndarray:
+    """Create a ket |psi> as a vertical vector.
+
+    :param psi: Sequence of complex numbers.
+    :return: A vertical vector as numpy array.
+    """
+    psi = np.asarray(psi)
+
+    if len(psi.squeeze().shape) != 1:
+        raise ValueError("Wrong parameter psi.")
+    return psi.reshape((-1, 1))
+
+
+def basis(dim: int, index: int) -> np.ndarray:
+    """Create computational basis of dimension dim.
+
+    :param dim: Dimension of vector space.
+    :param index:
+    :return: A basis vector |d, i>.
+    """
+    if index >= dim:
+        raise ValueError("Parameter index out of range.")
+
+    vec = np.zeros(dim)
+    vec[index] = 1
+    return vec
+
+
+def dagger(matrix: np.ndarray) -> np.ndarray:
+    """Create conjugate transpose of a given matrix.
+
+    :param matrix: A matrix given by numpy array.
+    :return: An operator dagger as concatanation conjugate and transpose.
+    """
+    return np.transpose(np.conjugate(matrix))
+
+
+def bra(psi: Array1D) -> np.ndarray:
+    """Create bra <psi| as a horizontal vector.
+
+    :param psi: Sequence of complex numbers.
+    :return: A horizontal vector as numpy array satisfying <psi| = |psi>^+
+    """
+    return dagger(ket(psi))
+
+
+def braket(psi: Array1D, phi: Array1D) -> complex:
+    """Create braket as an inner product between vectors psi and phi.
+
+    :param psi: Sequence of complex numbers.
+    :param phi: Sequence of complex numbers.
+    :return: Inner product between vectors psi and phi.
+    """
+    return complex(bra(psi) @ ket(phi))
+
+
+def ketbra(psi: Array1D, phi: Array1D) -> np.ndarray:
+    """Compute outer product |psi><phi|.
+
+    :param psi: Sequence of complex numbers.
+    :param phi: Sequence of complex numbers.
+    :return: A matrix |psi><phi|
+    """
+    return ket(psi) @ bra(phi)
+
+
+def normaliced_vector(psi: Array1D) -> np.ndarray:
+    """Create a normalized vector |psi>.
+
+    :param psi: Sequence of complex numbers.
+    :return: A normaliced vector |psi> / |||psi>||.
+    """
+    return ket(psi / linalg.norm(psi))
+
+
+def proj(psi: Array1D) -> np.ndarray:
+    """Create a projector onto normalized vector |psi>.
+
+    :param psi: Sequence of complex numbers.
+    :return: A matrix |psi><psi|.
+    """
+    return ket(normaliced_vector(psi)) @ bra(normaliced_vector(psi))
+
+
+def res(matrix: np.ndarray) -> np.ndarray:
+    """Create reshaping maps matrix into a vector row by row.
+    See res(A) as equivalence to vec(A.T).
+
+    :param matrix: A matrix to be reshaped.
+    :return: A vertical vector creating by matrix reshaping.
+    """
+    return ket(np.reshape(np.asarray(matrix), np.size(matrix)))
+
+
+def unres(psi: Array1D, dims: typing.Tuple[int, int]) -> np.ndarray:
+    """Un-reshaping of the vector into the matrix of dimension dims.
+
+    :param psi: A matrix given by numpy array.
+    :param dims: A tuple of dimensions.
+    :return: A vertical vector creating by matrix vectorization.
+    """
+    return np.reshape(np.asarray(psi), dims)
+
+
+def vec(matrix: np.ndarray) -> np.ndarray:
+    """Create reshaping maps matrix into a vector column by column. vec(A) is equivalent to res(A.T).
+
+    :param matrix: A matrix to be vectorized.
+    :return: A matrix of dimension dims.
+    """
+    return ket(np.reshape(np.asarray(matrix).T, np.size(matrix)))
+
+
+def unvec(psi: Array1D, dims: typing.Tuple[int, int]) -> np.ndarray:
+    """Re-vectorization of the vector into the matrix of dimension dims.
+
+    :param psi: A matrix given by numpy array.
+    :param dims: A tuple of dimensions.
+    :return: A matrix of dimension dims.
+    """
+
+    return np.asarray(np.reshape(np.asarray(psi), dims)).T
+
+
+def bell_state(qubits: int = 2) -> np.ndarray:
+    """Create the maximally entangled state of dimension 2 ** qubits.
+
+    :param qubits: Number of qubits.
+    :return: The maximally entangled state of dimension 2 ** qubits.
+    """
+    return vec(np.identity(qubits)) / linalg.norm(vec(np.identity(qubits)))
+
+
+def bell_state_density(qubits: int = 2) -> np.ndarray:
+    """Create the maximally entangled state of dimension 2 ** qubits as density operator.
+
+    :param qubits: Number of qubits.
+    :return: The maximally entangled state of dimension 2 ** qubits as density operator.
+    """
+    return proj(vec(np.identity(qubits)))
--- tests/test_quit.py
+++ tests/test_quit.py
@@ -1,5 +1,136 @@
-from quit import dummy
+import numpy as np
+import pytest
+from scipy import linalg
+
+from quit.basicfunction import (
+    basis,
+    bell_state,
+    bell_state_density,
+    bra,
+    braket,
+    dagger,
+    ket,
+    ketbra,
+    proj,
+    res,
+    unres,
+    unvec,
+    vec,
+)
 
 
-def test_dummy():
-    assert dummy() == 2137
+@pytest.mark.parametrize("phi", [np.pi, np.sqrt(2), 2j, 1 + 1j, 1])
+def test_ket_with_different_types_of_entries(phi):
+    np.testing.assert_array_equal(np.array([[1.0], [phi]]), ket([1, phi]))
+
+
+@pytest.mark.parametrize("dim", [4, 8])
+@pytest.mark.parametrize("index", [2, 3])
+def test_if_basis_is_correctly_defined(dim, index):
+    np.testing.assert_array_equal(basis(dim, index), np.identity(dim)[index])
+
+
+@pytest.mark.parametrize("phi", [np.pi, np.sqrt(2), 2j, 1 + 1j, 1])
+def test_bra_with_different_types_of_entries(phi):
+    np.testing.assert_array_equal(np.array([[1, np.conjugate(phi)]]), bra([1, phi]))
+
+
+@pytest.mark.parametrize("phi", [[np.sqrt(2), 1j + 2]])
+@pytest.mark.parametrize("psi", [[np.pi, 1, 2j]])
+def test_ketbra_is_equal_outer_product(phi, psi):
+    np.testing.assert_almost_equal(ketbra(phi, psi), np.outer(phi, np.conjugate(psi)))
+
+
+@pytest.mark.parametrize("phi", [[np.sqrt(2), 1j + 2]])
+@pytest.mark.parametrize("psi", [[np.pi, 2j]])
+def test_braket(phi, psi):
+    np.testing.assert_almost_equal(braket(phi, psi), np.inner(np.conjugate(phi), psi))
+
+
+@pytest.mark.parametrize("vector", [[1, 2], [1, -1]])
+def test_projector_is_idemotent(vector):
+    np.testing.assert_almost_equal(proj(vector) @ proj(vector), proj(vector))
+
+
+sx = np.array([[0, 1], [1, 0]])
+
+
+@pytest.mark.parametrize(
+    "symmatrix", [np.identity(3), np.asarray(linalg.hadamard(2, dtype=complex)), np.kron(sx, sx)]
+)
+def test_dagger_for_hermitian_matrices_is_equal_its_dagger_operation(symmatrix):
+    np.testing.assert_array_equal(dagger(symmatrix), symmatrix)
+
+
+def test_function_res_naive():
+    matrix = np.array([[1, 1 - 1j], [0, 1], [np.pi, -1]])
+    np.testing.assert_array_equal(res(matrix), ket(np.asarray([1, 1 - 1j, 0, 1, np.pi, -1])))
+
+
+def test_function_res_by_using_telegraphic_notation():
+
+    a = np.array([[1, 1], [2, 2]])
+    b = np.identity(2)
+    c = np.array([[-1, 1], [0, 2]])
+
+    np.testing.assert_array_equal(np.kron(a, b) @ res(c), res(a @ c @ np.transpose(b)))
+
+
+def test_function_res_for_rank_one_operator():
+    x = [1, 1 - 1j]
+    y = [np.pi, -1]
+    np.testing.assert_array_equal(res(ketbra(x, y)), ket(np.kron(x, y)))
+
+
+def test_function_res_for_vector():
+    x = [1, 1 - 1j, np.pi, -1]
+    np.testing.assert_array_equal(res(x), ket(x))
+
+
+def test_function_vec_naive():
+    matrix = np.array([[1, 1 - 1j], [0, 1], [np.pi, -1]])
+    np.testing.assert_array_equal(vec(matrix), ket(np.asarray([1, 0, np.pi, 1 - 1j, 1, -1])))
+
+
+def test_function_vec_as_transposition_of_res():
+    matrix = np.array([[1, 1 - 1j], [0, 1], [np.pi, -1]])
+    np.testing.assert_array_equal(vec(matrix), res(np.transpose(matrix)))
+
+
+sx = np.array([[0, 1], [1, 0]])
+
+
+@pytest.mark.parametrize(
+    "symmatrix", [np.identity(3), np.asarray(linalg.hadamard(2, dtype=complex)), np.kron(sx, sx)]
+)
+def test_if_vec_and_res_is_equal_on_symmetric_matrix(symmatrix):
+    np.testing.assert_array_equal(vec(symmatrix), res(symmatrix))
+
+
+def test_unres():
+
+    vector = np.array([[1, 1 - 1j, 0, 1, np.pi, -1]])
+    matrix = np.array([[1, 1 - 1j], [0, 1], [np.pi, -1]])
+
+    np.testing.assert_array_equal(unres(vector, (3, 2)), matrix)
+
+
+def test_unvec():
+
+    vector = np.array([[1, 1 - 1j, 0, 1, np.pi, -1]])
+    matrix = np.array([[1, 1 - 1j], [0, 1], [np.pi, -1]])
+
+    np.testing.assert_array_equal(unvec(vector, (3, 2)), matrix.transpose())
+
+
+def test_bell_state():
+
+    np.testing.assert_array_almost_equal(bell_state(2), 1 / np.sqrt(2) * ket([1, 0, 0, 1]))
+
+
+@pytest.mark.parametrize("dim", [2, 3, 4])
+def test_bell_state_density(dim):
+    np.testing.assert_array_almost_equal(
+        bell_state_density(dim),
+        ketbra(vec(np.identity(dim) / np.sqrt(dim)), vec(np.identity(dim) / np.sqrt(dim))),
+    )
